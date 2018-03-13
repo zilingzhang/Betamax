e701e6885742644f396df15985559bbcf3de56d9
jstenerson <jstenerson@mymail.mines.edu>
2018-03-13 09:52:18 -0600

    added documents of login

--- Betamax/urls.py
+++ Betamax/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
 
 #Add Django site authentication urls (for login, logout, password management)
 urlpatterns += [
-    path('accounts/', include('django.contrib.auth.urls')),
+    url(r'^accounts/', include('django.contrib.auth.urls'))
 ]
+
